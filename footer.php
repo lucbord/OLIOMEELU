96b64cd39e98679ab2590daf922fc187b0ca0a0d
massimoserpilli <massimo.serpilli@gmail.com>
2014-07-29 09:45:07 +0200

    Final Release Home Page

--- footer.php
+++ footer.php
@@ -1,26 +1,7 @@
 	<footer class="col-lg-12 col-xs-12" >
-	
-	<div class="col-md-4">
-		<h2>Ristorante Ciupin</h2>
-
-		<p>Via Angelo Argiroffo 19<br>
-		16043 Chiavari, Genova<br>
-		Tel.: +39 0185 322 151<br>
-		Mobile: +39 348 2665635<br>
-		P.iva 0123456789123
-		</p>
-		<p>Email: <a href="mailto:ristorante@ciupin.com">ristorante@ciupin.com</a></p>
-
-	</div>
-
-	<div class="col-md-4">
-			
-	</div>
-
-	<div class="col-md-4">
-		
-	</div>
-
+		<p>ANTICO FRANTOIO PANIZZI GIOVANNI BATTISTA VIA G.B.BOERI, 50 
+                    - 18010 BADALUCCO TEL.0184.408097  FAX: 0184 956722  CELL:334 1161668
+                </p>
 	</footer> 
 
 	</div>   
